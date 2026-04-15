b959d06e1d28a7d8cf3b571dcba3105fed03bdff
jerome <depuytjerome@gmail.com>
2026-04-15 14:09:09 +0200

    WIP

--- demo/10-redux-ajax/src/App.js
+++ demo/10-redux-ajax/src/App.js
@@ -1,21 +1,21 @@
-import { useDispatch } from "react-redux";
+/* import { useDispatch } from "react-redux"; */
 import "./App.css";
 import MainFooter from "./containers/main-footer/main-footer";
 import MainHeader from "./containers/main-header/main-header";
-import { fetchLiveboard } from "./store/actions/erail-action";
+/* import { fetchLiveboard } from "./store/actions/erail-action"; */
 
 function App() {
-  const dispatch = useDispatch();
+/*   const dispatch = useDispatch();
 
   const handleRequest = () => {
     dispatch(fetchLiveboard("simonis"));
-  };
+  }; */
 
   return (
     <>
       <MainHeader />
       <main className="App">
-        <button onClick={handleRequest}>console.log</button>
+        <button /*onClick={handleRequest}*/>console.log</button>
       </main>
       <MainFooter />
     </>
